b00f45455ecdf3b8bba26ed62220bbf29cf33565
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-03-02 14:26:53 +0530

    new file

--- 02-variables.sh
+++ 02-variables.sh
@@ -8,4 +8,4 @@ echo  "$PERSON1: hello $PERSON2 how are u"
 echo  "$PERSON2: hi $PERSON1 fine how abou u ?"
 echo  "$PERSON1: what is your plan for weekend"
 echo  "$PERSON2: I dont have any plans $PERSON1"
-echo  "$PERSON1: how about u $PERSON
+echo  "$PERSON1: how about u $PERSON2
