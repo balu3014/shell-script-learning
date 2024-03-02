f957ede8e3e08ac1beaec2057c8c6fd018f8cf5e
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-03-02 14:12:15 +0530

    new file

---
+++ 01-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo  "suresh: hello ramesh how are u"
+echo  "ramesh: hi suresh fine how abou u ?"
+echo  "suresh: what is your plan for weekend"
+echo  "ramesh: I dont have any plans suresh"
